9e955d5a74f572857ffd2c1d02bfe1d54712b5ad
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-07-18 15:08:51 +0200

    Post 3.15.0b4

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -27,7 +27,7 @@
 #define PY_RELEASE_SERIAL       4
 
 /* Version as a string */
-#define PY_VERSION              "3.15.0b4"
+#define PY_VERSION              "3.15.0b4+dev"
 /*--end constants--*/
 
 
